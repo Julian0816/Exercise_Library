c281154653825e77cc9f99a6f843ef325ab7eab9
Julian0816 <83423059+Julian0816@users.noreply.github.com>
2023-05-24 17:51:42 +0100

    Change Index and Erro_page

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,6 +1,6 @@
 import { useRouteError } from "react-router-dom";
 import { styled } from "@mui/system";
-import {Container} from "@mui/system";
+import { Container, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 
@@ -9,6 +9,20 @@ const CenteredContainer = styled(Container)`
   justify-content: center;
   align-items: center;
   height: 100vh;
+  background: linear-gradient(#cee5f2, #f2f5f7);
+`;
+
+const StyledButton = styled(Button)`
+  background-color: #536b78;
+  color: #fff;
+  padding: 12px 24px;
+  border-radius: 4px;
+  font-weight: bold;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #40555f;
+  }
 `;
 
 export default function ErrorPage() {
@@ -22,7 +36,9 @@ export default function ErrorPage() {
       <div id="error-page">
         <h1>Oops!</h1>
         <p>Sorry, an unexpected error has occurred.</p>
-        <p onClick={() => {navigate('/')}}> -- Click me to go back to the main page -- </p>
+        <StyledButton variant="contained" onClick={() => navigate("/")}>
+          Go back to the home page
+        </StyledButton>
         <p>
           <i>{error.statusText || error.message}</i>
         </p>
